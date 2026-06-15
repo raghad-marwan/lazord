e7da7ec66e641d40f388e6ccca5b624bd84f99d2
Raghad Abu Dawabeh <youremail@example.com>
2026-06-15 12:51:35 +0300

    fix: force https and build assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -17,8 +17,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+
+
+public function boot()
+{
+    // إجبار المتصفح على تحميل ملفات الـ CSS والصور بشكل آمن أونلاين
+    if (app()->environment('production') || env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
     }
+}
 }
